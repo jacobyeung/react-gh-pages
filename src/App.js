39c6127aa5196774bdf2e52ee4a421035d42b371
Jacob Yeung <jacobyeung@berkeley.edu>
2019-07-09 00:01:18 -0700

    Added source code button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navbar from './Components/Navbar'
+import Button from '@material-ui/core/Button'
 import About from './Components/About';
 import Education from './Components/Education'
 import './App.css';
 import Experience from './Components/Experience';
 import Skills from './Components/Skills';
+import source from './Images/sourcecode.png'
+
 
 function App() {
   return (
@@ -23,7 +26,10 @@ function App() {
         <Education/>
         <Experience/>
         <Skills/>
-        
+        <Button style={{alignItems: 'flex-start', display: 'flex'}}onClick={() => window.open('https://github.com/jacobyeung/react-gh-pages')}>
+          <img src={source} style={{height: '10vh'}}/> <br/>
+          Source Code
+        </Button>
       </div>
     </div>
   );
