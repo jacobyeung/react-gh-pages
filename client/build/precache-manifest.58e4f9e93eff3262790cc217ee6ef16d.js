d71b4f9ae13e0c1b42c4f33ec121be95ebe8c9b3
Jacob Yeung <jacobyeung@Jacobs-MacBook-Pro.local>
2019-07-25 22:48:02 -0700

    Second try for backend

---
+++ client/build/precache-manifest.58e4f9e93eff3262790cc217ee6ef16d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab93e9f509e839847ae76e5565ea1d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "146c4b026f61bd942540",
+    "url": "/static/css/main.523971bc.chunk.css"
+  },
+  {
+    "revision": "0a040c9ca931be23bfdb",
+    "url": "/static/js/2.d7cec453.chunk.js"
+  },
+  {
+    "revision": "146c4b026f61bd942540",
+    "url": "/static/js/main.3228a231.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "728aaff84083892dfa50b7f2e87d4e24",
+    "url": "/static/media/berkeley.728aaff8.png"
+  },
+  {
+    "revision": "4ed8086a0eedcb0796152e03d447306b",
+    "url": "/static/media/cisco.4ed8086a.png"
+  },
+  {
+    "revision": "5cfdcd7e4f8619007771f1475ef27029",
+    "url": "/static/media/davis.5cfdcd7e.png"
+  },
+  {
+    "revision": "eedd8a412f057a24911b5f39eab02131",
+    "url": "/static/media/jacobyeung.eedd8a41.jpg"
+  },
+  {
+    "revision": "ac0686a4662c1e4643d42c1946bb84f1",
+    "url": "/static/media/mern.ac0686a4.png"
+  },
+  {
+    "revision": "72c0289088ac6304dda0cabfd772cdb4",
+    "url": "/static/media/python.72c02890.png"
+  }
+]);
